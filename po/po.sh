0792979eb2dc96d6b26ce8d0cec20bdac3949879
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2007-01-02 15:22:03 +0000

    knx-installer (0.4.1.3) unstable; urgency=low
      * depend on procps.
      * rename main installer to knx-installer.
      * add sidux-installer wrapper.
      * fix defaults for sidux.
    
     -- Stefan Lippers-Hollmann <s.l-h@gmx.de>  Tue,  2 Jan 2007 16:21:48 +0100

---
+++ po/po.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+{
+bash --dump-po-strings knx-installer
+
+for i in $(find modules/ -name "*.bm")
+do
+  bash --dump-po-strings $i
+done
+
+for i in $(find tools/ -type f )
+do
+  bash --dump-po-strings $i
+done
+
+} | msguniq > po/messages.pot
